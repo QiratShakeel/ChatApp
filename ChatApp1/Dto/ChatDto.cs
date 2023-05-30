d4c39c5895a99074164f620222a0a11db8d97d0d
QiratShakeel <qiratshakeel09@gmail.com>
2023-05-30 17:52:40 +0500

    Chat App

---
+++ ChatApp1/Dto/ChatDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatApp1.Dto
+{
+    public class ChatDto
+    {
+        public int Id { get; set; }
+        public string User_Email { get; set; }
+        public string Message { get; set; }
+        public string Reciever { get; set; }
+    }
+}
---
+++ ChatApp1/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using ChatApp1.Dto;
+using Microsoft.AspNetCore.SignalR;
+using System.Text.RegularExpressions;
+
+namespace ChatApp1.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            HttpContext httpContext = Context.GetHttpContext();
+
+            string receiver = httpContext.Request.Query["userid"];
+            string sender = Context.User.Claims.FirstOrDefault().Value;
+
+            Groups.AddToGroupAsync(Context.ConnectionId, sender);
+            if (!string.IsNullOrEmpty(receiver))
+            {
+                Groups.AddToGroupAsync(Context.ConnectionId, receiver);
+            }
+
+            return base.OnConnectedAsync();
+        }
+        public Task SendMessageToGroup(ChatDto chat)
+        {
+            return Clients.Group(chat.Reciever).SendAsync("ReceiveMessage"
+                , Context.User.Identity.Name , chat.Message);
+        }
+    }
+}
